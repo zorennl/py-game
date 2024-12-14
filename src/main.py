095802ca92180485b7619cae49b08ff896643eab
zorennl <zoren@leit.so>
2024-12-13 20:41:00 -0800

    attempting to add keypress detection

--- src/main.py
+++ src/main.py
@@ -5,12 +5,12 @@ pygame.display.flip()
 
 pygame.display.set_caption("First Game")
 
-rect = pygame.Rect(10, 20, 30, 30)
-surface = pygame.image.load('/home/zmynz/vss/python/first-game/assets/textures/player.png')
-
 running = True
+
 while running:
     for event in pygame.event.get(): 
         # Check for QUIT event       
         if event.type == pygame.QUIT: 
+            running = False
+        if event.key == pygame.K_q:
             running = False
